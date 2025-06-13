247f2f81fa672911da3e1acf447348a5ad4bd212
Choongman Kim <cm730712@gmail.com>
2025-06-13 19:07:47 +0900

    Modify: 메타데이터 업데이트 및 이미지 경로 변경
    - 포트폴리오 제목을 'Fullkeem | Web Developer'로 수정
    - 메타데이터의 이미지 경로를 '/images/default.png'로 변경
    - 트위터 카드 제목을 'Fullkeem | web developer'로 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,11 +39,11 @@ export const metadata: Metadata = {
     locale: 'ko_KR',
     url: 'https://fullkeem.dev',
     siteName: 'fullkeem Portfolio',
-    title: 'Fullkeem | fullkeem',
+    title: 'Fullkeem | Web Developer',
     description: '랜딩 페이지 제작 전문 프론트엔드 개발자',
     images: [
       {
-        url: '/og-image.png',
+        url: '/images/default.png',
         width: 1200,
         height: 630,
         alt: 'Fullkeem | fullkeem',
@@ -52,9 +52,9 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Fullkeem | fullkeem',
+    title: 'Fullkeem | web developer',
     description: '랜딩 페이지 제작 전문 프론트엔드 개발자',
-    images: ['/og-image.png'],
+    images: ['/images/default.png'],
   },
   robots: {
     index: true,
